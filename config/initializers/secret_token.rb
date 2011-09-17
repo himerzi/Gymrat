a2f6a4452f3e3503eba7ec5444da6510d7a99568
Michael Detmold <michael.detmold@thatcrowdthing.com>
2011-09-17 17:56:21 +0100

    first commit, with mongoid installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gymrat::Application.config.secret_token = 'b32b14723922b58fb83ab09e8479fdd287eb0b4261db07f6aa71332741e9dca2bfbc76bb977caa53210a21db31a008a16829bd43d58e87925ced794d38b3a3a6'
